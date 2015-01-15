e829e513bbd5222f510873c13a320c04e7adc10f
nlivni <nathanlivni@gmail.com>
2015-01-15 13:39:00 -0600

    content search() now takes property to be searched for. Activities are objects which share id strings with activity function names

--- templates/activities.js
+++ templates/activities.js
@@ -1,5 +1,14 @@
 // action names are stored as strings Person objects
 
+function Activity(id, name, description, connection) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.connection = connection;
+}
+var activity_deck = [];
+activity_deck.push(new Activity('monster_dance', 'Monster Dance', 'Dance with a monster', 0));
+
 // monster befriends sad kids and bites happy ones
 function monster_dance(person) {
     var output = person.name + " invites you under the bed to dance the monster dance.";
@@ -15,36 +24,42 @@ function monster_dance(person) {
     return output;
 };
 
+activity_deck.push(new Activity('kill', 'Play a little game', 'Your monster friend has an evil look in his eyes', 0));
 function kill(person) {
     player.health += 1;
     var output = "PLACEHOLDER TEXT FOR MONSTER KILL";
     return output;
 };
 
+activity_deck.push(new Activity('baby_talk', 'Baby Talk', 'Your monster friend has an evil look in his eyes', 0));
 function baby_talk(person) {
     player.health += 1;
     var output = person.name + " talk babytalk to you. Your health = " + player.health;
     return output;
 };
 
+activity_deck.push(new Activity('baby_feeding', 'Baby Feeding', 'Your monster friend has an evil look in his eyes', 0));
 function baby_feeding(person) {
     player.health += 1;
     var output = person.name + " feed you. Your health = " + player.health;
     return output;
 };
 
+activity_deck.push(new Activity('babysitting', 'Babysitting', 'Your monster friend has an evil look in his eyes', 0));
 function babysitting(person) {
     player.health += 1;
     var output = person.name + " babysits you. Your health = " + player.health;
     return output;
 };
 
+activity_deck.push(new Activity('smoking', 'Smoking', 'Your monster friend has an evil look in his eyes', 0));
 function smoking(person) {
     player.health += 1;
     var output = person.name + " offers you a cigarrette. Your health = " + player.health;
     return output;
 };
 
+activity_deck.push(new Activity('drinking', 'Drinking', 'Your monster friend has an evil look in his eyes', 0));
 function drinking(person) {
     player.happiness += 1;
     player.health -= 1;
@@ -58,18 +73,21 @@ function drinking(person) {
     return output;
 };
 
+activity_deck.push(new Activity('partying', 'Partying', 'Your monster friend has an evil look in his eyes', 0));
 function partying(person) {
     player.happiness += 1;
     var output = person.name + " and you go partying. Your happiness = " + player.happiness;
     return output;
 };
 
+activity_deck.push(new Activity('sex', 'Sex', 'Your monster friend has an evil look in his eyes', 0));
 function sex(person) {
     player.health += 1;
     var output = person.name + " and you have sex. Your health = " + player.health;
     return output;
 };
 
+activity_deck.push(new Activity('dating', 'Dating', 'Your monster friend has an evil look in his eyes', 0));
 function dating(person) {
     player.health += 1;
     var output = person.name + " and you have sex. Your health = " + player.health;

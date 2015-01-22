b7fc378053c1757c10a9582b18b361c115062697
Unpaid-Intern <scottybrackett@gmail.com>
2015-01-21 23:15:01 -0600

    adds rendering transitions working animation and sound effects to button presses. The sound currently in use is a placeholder, as it gets annoying pretty quickly.

---
+++ templates/SFX.js
@@ -0,0 +1,9 @@
+/**
+ * Created by currentuser on 1/21/15.
+ */
+$( document ).on("click", ".choice-button", function(){
+
+    var audio = new Audio('templates/audio/muted_beep_mod.mp3');
+    audio.play();
+
+});

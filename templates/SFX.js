8773d462bf43bb3799b2fbe23c797b15cd4b9aae
Unpaid-Intern <scottybrackett@gmail.com>
2015-02-01 20:07:49 -0600

    added Chris's SFX for health_down, and choice button.

--- templates/SFX.js
+++ templates/SFX.js
@@ -1,15 +1,13 @@
 /*Created by currentuser on 1/21/15.*/
 //will need to trigger this sound with choices.js code instead of event listener
-function soundTrigger(){
-
-    if(_ShakeTrigger == true) {
-        var audio = new Audio('templates/audio/muted_beep_mod.mp3');
-    }
-    else{
-        var audio = new Audio('templates/audio/electribe_click.mp3');
-    }
+function soundTrigger()
+    {
+        if(_ShakeTrigger == true) {
+            var audio = new Audio('templates/audio/Health_Down.mp3');
+        }
+        else{
+            var audio = new Audio('templates/audio/Select_Choice.mp3');
+        }
     audio.play();
-
-
-}
+    }
 
